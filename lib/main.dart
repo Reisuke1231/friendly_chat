f32d97b6ed17698cee528dcc45a56037507cdf87
Reisuke1231 <reisuke1231@gmail.com>
2022-03-29 11:30:37 +0900

    Build the main user interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:friendly_chat/screens/chat_screen.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      title: 'FriendlyChat',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('FriendlyChat'),
-        ),
-      ),
-    ),
+    FriendlyChatApp(),
   );
+}
+
+class FriendlyChatApp extends StatelessWidget {
+  const FriendlyChatApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FriendlyChat',
+      home: ChatScreen(),
+    );
+  }
 }
